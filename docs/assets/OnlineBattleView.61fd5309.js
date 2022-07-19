321f010fe1f9261e9c49c238648f8b4c36b3c7b4
fummicc1 <fumiya.tennis1@gmail.com>
2022-07-19 19:27:08 +0900

    Use gh-pages

--- docs/assets/OnlineBattleView.61fd5309.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c}from"./index.12930572.js";const t={};function o(r,a){return n(),c("div",null,"Online")}const s=e(t,[["render",o]]);export{s as default};
--- docs/assets/OnlineBattleView.c22f3571.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c}from"./index.8c8eaaec.js";const t={};function o(r,a){return n(),c("div",null,"Online")}const s=e(t,[["render",o]]);export{s as default};
